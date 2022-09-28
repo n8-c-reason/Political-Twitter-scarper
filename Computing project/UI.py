8413d49665d1cb77cd7838f530015ab8a1c073c8
n8-c-reason <nate.conway.reason@gmail.com>
2022-09-28 15:15:46 +0100

    Added qss sheet

--- Computing project/UI.py
+++ Computing project/UI.py
@@ -1,5 +1,39 @@
 import sys ##Allows you to launch the app from commandline 
-from PyQt6.QtWidgets import (QMainWindow, QApplication) ##All the widgets I may use I'll import here
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QGridLayout) ##All the widgets I may use I'll import here
+
+
+##This is the subclass for our QMain window
+class MainWindow(QMainWindow):##Everything will be contained within this widget.
+    def __init__(self): ##When you make a subclass for a Qt class we always need to call a super init function
+        ##This alows Qt to create an object
+        super().__init__()
+
+        ##This allows us to change the title of our window
+        self.setWindowTitle("Python Twitter scraper")##This is aimed at self(MainWindow)
+
+        self.primaryMenue()## Calls the function for the primary menue
+
+        self.setCentralWidget(self.mainWidget) ## Sets the the window to the primary menue through the main widget
+        ##change this if you want to change menue ^
+
+    def primaryMenue(self):## The function which will hold all the widgets for the primary menue
+        ##The layout I shall use for this menue is the gridlayout
+        self.gMainLayout = QGridLayout()##This allows me to split my window in to multile pices
+
+        self.centralTitle = QLabel("Tweet sentiment analysier") ##Creates a Qlabel which I'll use for my main title
+        self.centralTitle.setProperty("class", "title")
+        self.gMainLayout.addWidget(self.centralTitle, 3,3)##Adds the label to the layout at the postion (3,3)
+
+        self.mainWidget = QWidget()  ## This creates the widget which holds the primary menue
+        self.mainWidget.setLayout(self.gMainLayout) ## This attaches the layout to this widget
+
+
+
+
+
+
+        
 
 
 if __name__ == '__main__': ##Starts the program
@@ -8,9 +42,14 @@ if __name__ == '__main__': ##Starts the program
     app = QApplication(sys.argv) ##An object which holds the event loop
 
     ##Creates my first  top level widget widnow
-    main_menue = QMainWindow() ## In this case it is QMainWindow
+    mainMenue = MainWindow() ## In this case it is QMainWindow
 
-    main_menue.show() ##All widgets need to be shown
+    mainMenue.show() ##All widgets need to be shown
+    
+    ## Opens the main style sheet for the project
+    with open("main style sheet.qss", "r") as f: 
+        _style = f.read() 
+        app.setStyleSheet(_style)##Sets the style sheet
 
     ##Starts the loop
     app.exec()
